9d47ae40c38e1500aacf4f47fa1d9bd630cc5251
dalia sawaya <dalia.sawaya@flatironschool.com>
2020-03-24 15:03:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+
+function findMatching(drivers, name){
+  return drivers.filter(d =>
+    d.toUpperCase() === name.toUpperCase()
+  )
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter(d =>
+    d[0] === name[0]
+  )
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(d =>
+  d.name === name
+  )
+}
+
+// function findMatching(source, sought) {
+//   return source.filter( possibleMatch =>
+//     possibleMatch.toLowerCase() === sought.toLowerCase()
+//   )
+// }
