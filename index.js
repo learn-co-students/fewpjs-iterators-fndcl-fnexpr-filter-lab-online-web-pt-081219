c3b43a134e3b4dbaf7db72d4e028669de44f0943
saramccombs <theSaraMcCombs@gmail.com>
2020-03-28 18:42:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(arrayOfNames, nameToFind) {
+  let results = arrayOfNames.filter(name => name.toLowerCase() === nameToFind.toLowerCase()) 
+  return results
+}
+
+function fuzzyMatch(arrayOfNames, beginningOfName) {
+  let results = arrayOfNames.filter(name => name.toLowerCase().indexOf(beginningOfName.toLowerCase()) === 0)
+  return results
+}
+
+function matchName(arrayOfNames, nameToFind) {
+  let results = arrayOfNames.filter(n => n.name === nameToFind)
+  return results
+}
